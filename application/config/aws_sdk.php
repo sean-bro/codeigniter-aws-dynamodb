c69a47903736eadae7048e68349fd0d5c2eeb9f6
sean-bro <seanbro@gmail.com>
2018-07-13 14:30:59 -0700

    Update aws_sdk.php

--- application/config/aws_sdk.php
+++ application/config/aws_sdk.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['aws_access_key']		= 'Your Access Key';
-$config['aws_secret_key']		= 'Your Secret Key';
+$config['aws_access_key']	= 'Your Access Key';
+$config['aws_secret_key']	= 'Your Secret Key';
 $config['aws_region_default']	= 'us-west-2';
